08c3f9d63f133d6ca825ddb97a97e6347641af89
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-09-23 22:48:46 +0200

    se mejora el código con más condiciones

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,25 +1,35 @@
 /*
-Diseñar un algoritmo que tomando las coordenadas 
-cartesianas de un punto en el
-plano y calcule e imprima el cuadrante al que 
-pertenece dicho punto. También debe
-detectar cuando está en el origen o sobre un eje.
+Diseñar un algoritmo que tomando las coordenadas cartesianas de un 
+punto en el plano y calcule e imprima el cuadrante al que pertenece dicho 
+punto. También debe detectar cuando está en el origen o sobre un eje.
 */
 
-//coordenadas del punto
-var cX= 4;
-var cY= -1;
+//creamos las dos  variables que representa x, y
 
-//determinamos a que cuadrante pertenece el punto
-if (cX > 0 && cY > 0) {
-    alert("El punto se encuentra en el cuadrante 1 y sobre su eje");
-  } else if (cX< 0 && cY > 0) {
-    alert("El punto se encuentra en el cuadrante 2 y sobre su eje");
-  } else if (cX< 0 && cY < 0) {
-    alert("El punto se encuentra en el cuadrante 3 y sobre su eje");
-  } else if (cX > 0 && cY < 0) {
-    alert("El punto se encuentra en el cuadrante 4 y sobre su eje");
-  } else {
-    alert("El punto está en su origen");
-  }
+/*ejemplos: x=3, y=4  / el punto esta en el primer cuadrante
+( x = -5 ), ( y = 2 )  “El punto está en el segundo cuadrante”
+ ( x = -3 ), ( y = -7 )  “El punto está en el tercer cuadrante”
+  ( x = 6 ), ( y = -1 )  “El punto está en el cuarto cuadrante”
+
+
+*/
+
+var x = -5, y = 2;
+
+//determinamos donde se encuentran los puntos
+if (x === 0 && y === 0) {
+    alert('X y Y están en el origen');
+} else if (x === 0) {
+    alert('El punto está sobre el eje Y');
+} else if (y === 0) {
+    alert('El punto está sobre el eje X');
+} else if (x > 0 && y > 0) {
+    alert('El punto está en el primer cuadrante');
+} else if (x < 0 && y > 0) {
+    alert('El punto está en el segundo cuadrante');
+} else if (x < 0 && y < 0) {
+    alert('El punto está en el tercer cuadrante');
+} else if (x > 0 && y < 0) {
+    alert('El punto está en el cuarto cuadrante');
+}
   
