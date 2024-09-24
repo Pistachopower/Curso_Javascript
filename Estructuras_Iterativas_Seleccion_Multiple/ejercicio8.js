88f63b8381fa30721a4de0b6d03973d97f40afd9
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-09-24 18:55:45 +0200

    Se hace otro repaso y se busca otra solucion en el ejercicio9

--- Estructuras_Iterativas_Seleccion_Multiple/ejercicio8.js
+++ Estructuras_Iterativas_Seleccion_Multiple/ejercicio8.js
@@ -1,18 +1,41 @@
 /*
-Diseñar un algoritmo para escribir en pantalla los n primeros números naturales, sus
-cuadrados, sus cubos y la suma de todos ellos.
+Diseñar un algoritmo para escribir en pantalla los n 
+primeros números naturales, sus cuadrados, sus cubos 
+y la suma de todos ellos.
+
+    console.log("Cuadrado: " + Math.pow(i,2));
+
+    //calculo y muestro sus cubos
+    console.log("Al cubo: " + Math.pow(i,3));
 */
 
-var n= 2;
-var contador=0;
+//variable n
+var n = 5;
 
+
+//variable suma a 0
+var suma = 0;
+
+//estructura for recorre de n a 1
 for (let i = 1; i <= n; i++) {
-    // Se ejecuta 5 veces, con valores del paso 0 al 4.
-    console.log(i);
-    console.log("Cuadrado: ", Math.pow(i,2));
-    console.log("Cubo : ", Math.pow(i,3));
-    contador+= i;
-    
-  }
-  console.log("Suma: ", contador);
-  
+    //creamos las variables locales que existen en este ámbito
+    let n_Cuadrado = 0;
+    let n_Cubo = 0;
+
+    //muestro los números
+    console.log("Numero: " + i);
+
+    //calculo sus cuadrados 
+    n_Cuadrado = n_Cuadrado + Math.pow(i, 2);
+
+    //calculo sus cubos
+    n_Cubo = n_Cubo + Math.pow(i, 3);
+
+    //sumo los valores que arroja n
+    suma = suma + i + n_Cuadrado + n_Cubo;
+
+
+}
+
+//muestro la suma de todos
+console.log("Suma total: " + suma);
--- Estructuras_Iterativas_Seleccion_Multiple/ejercicio9.js
+++ Estructuras_Iterativas_Seleccion_Multiple/ejercicio9.js
@@ -1,6 +1,60 @@
 /*
 Hallar el menor, el mayor y la media de un conjunto de números positivos leídos de
 teclado.
+
+
+Otra solucion
+//cuenta los numeros
+var total_Numeros = 0;
+
+//suma para la media
+var suma = 0;
+
+var menor=0;
+var mayor=0;
+var valor=0;
+
+//la longitud del conjunto
+var conjunto_Numeros = parseInt(prompt('Ingrese la longitud del conjunto: '));
+
+for (let i = 0; i < conjunto_Numeros; i++) {
+
+    //valor a determinar menor, mayo y media
+    valor = parseInt(prompt('Ingrese el número: '));
+
+    // en la primera iteración, inicializamos menor y mayor
+    if (i === 0) {
+        menor = valor;
+        mayor = valor;
+
+
+    } else {
+
+        //determinamos menor
+        if (valor < menor) {
+            menor = valor;
+        }
+
+        //determinamos mayor
+        if (valor > mayor) {
+            mayor = valor;
+        }
+
+
+    }
+
+    //cuento la cantidad de numeros
+    total_Numeros++;
+
+    //sumo el valor del usuario
+    suma = suma + valor;
+
+}
+
+alert("El número menor es " + menor);
+alert("El número mayor es " + mayor);
+alert("La media es " + (suma / total_Numeros));
+
 */
 
 var valor;
