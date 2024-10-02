51f15a3593988a02cfb58335c9462854438c19a5
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-02 12:48:52 +0200

    expresiones regulares

---
+++ Boletin_2/ejercicio7.js
@@ -0,0 +1,21 @@
+/*
+Diseñar un script que lea una dirección de email y la valide. No 
+se terminará hasta introducir una dirección correcta. Modificar el 
+ejercicio anterior para una vez validado el correo mostrar el usuario y 
+el servidor de correo en dos mensajes.
+
+usuario@dominio.ext
+
+*/ 
+
+
+const expresionRegular = /^[a-z0-9]+@[a-z0-9]+\.(com|es|net|org)$/;
+
+var direccion = prompt("Ingrese su correo:");
+
+while (direccion.indexOf('@') === -1 || direccion.indexOf('.') === -1 || !(direccion.endsWith(".com") || direccion.endsWith(".es") || direccion.endsWith(".net") || direccion.endsWith(".org"))) {
+    alert("Correo inválido. Debe contener '@', un dominio y una extensión válida (.com, .es, .net, .org).");
+    direccion = prompt("Ingrese su correo:");
+}
+
+alert("Correo válido: " + direccion);
