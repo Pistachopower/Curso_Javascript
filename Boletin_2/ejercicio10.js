7d1a4ab1ac2536c19b2b1a05830f97598c765f8e
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-03 14:43:19 +0200

    Por hacer

--- Boletin_2/ejercicio10.js
+++ Boletin_2/ejercicio10.js
@@ -4,4 +4,6 @@ los espacios duplicados.
 */
 
 
-var cadena= "Hola buenos dias";
+var cadena= "Hola  buenos  dias";
+
+var cadenaFinal= result = text.replace(/^\s+|\s+$/gm,'');
---
+++ Boletin_2/ejercicio11.js
@@ -0,0 +1,5 @@
+/*
+Diseña una expresión regular que valide contraseñas que contengan 
+al menos una letra mayúscula, una letra minúscula, un número y 
+un carácter especial, y que tengan entre 8 y 20 caracteres.
+*/
