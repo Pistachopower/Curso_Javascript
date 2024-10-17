ac44aab03a9335dc0bd55e817e45cd33f2edba0a
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-17 14:42:01 +0200

    Pendiente por hacer ejercicio de 6 en adelante

---
+++ Funciones/ejercicio9.js
@@ -0,0 +1,43 @@
+/*
+Escribe todas las funciones en ES5 (Estandar js) y con la notación 
+de función flecha de ES6. 
+Escribe una función llamada elMenor que acepte un 
+número variable de parámetros y devuelva el parámetro de menor 
+valor pasado a la función.
+Escribe una función llamada colocaEnMedio que acepte como parámetros
+ dos arrays y devuelva el primer array con todos los valores del
+  segundo array colocados en el centro del primer array. 
+Ejemplos: 
+placeInMiddle([1,2,6,7],[3,4,5]) // [1,2,3,4,5,6,7]
+placeInMiddle([1],[3,4,5]) // [3,4,5,1]
+placeInMiddle([1,6],[2,3,4,5]) // [1,2,3,4,5,6]
+placeInMiddle([],[2,3,4,5]) // [2,3,4,5]
+
+
+Escribe una función llamada uneArrays que acepte un número variable
+de parámetros (cada uno de ellos será un array) y devuelva un nuevo
+array con todos los parámetros concatenados.
+		Ejemplos:
+joinArrays([1], [2], [3]); // [1,2,3]
+joinArrays([1,2,3],[4,5,6],[7,8,9]) //[1,2,3,4,5,6,7,8,9]
+
+
+Escribe una función llamada sumaArgPares que sume todos los argumentos pares que se pasen a la función.
+Ejemplos:
+sumEvenArgs(1,2,3,4) // 6
+sumEvenArgs(1,2,6) // 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
