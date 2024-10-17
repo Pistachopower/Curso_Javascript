56533d00b0f12e177b24ef3f41b47a500af2596b
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-17 22:59:47 +0200

    se hace ejercicio 9 por partes

--- Funciones/ejercicio9.js
+++ Funciones/ejercicio9.js
@@ -4,6 +4,7 @@ de función flecha de ES6.
 Escribe una función llamada elMenor que acepte un 
 número variable de parámetros y devuelva el parámetro de menor 
 valor pasado a la función.
+
 Escribe una función llamada colocaEnMedio que acepte como parámetros
  dos arrays y devuelva el primer array con todos los valores del
   segundo array colocados en el centro del primer array. 
@@ -17,7 +18,7 @@ placeInMiddle([],[2,3,4,5]) // [2,3,4,5]
 Escribe una función llamada uneArrays que acepte un número variable
 de parámetros (cada uno de ellos será un array) y devuelva un nuevo
 array con todos los parámetros concatenados.
-		Ejemplos:
+    Ejemplos:
 joinArrays([1], [2], [3]); // [1,2,3]
 joinArrays([1,2,3],[4,5,6],[7,8,9]) //[1,2,3,4,5,6,7,8,9]
 
@@ -27,17 +28,34 @@ Ejemplos:
 sumEvenArgs(1,2,3,4) // 6
 sumEvenArgs(1,2,6) // 8
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 */
+
+
+
+
+//forma 1
+//usamos el operador Rest porque no sabemos la cantidad de parametros
+var elMenor = function (...numeros) {
+  // Usamos Math.min para obtener el menor valor de los argumentos
+  var valores = Math.min(...numeros);
+
+  return valores;
+}
+
+//alert(elMenor(3, 8, 5, 1));
+
+//con funcion flecha
+var elMenor = (...numeros) => Math.min(...numeros);
+
+//alert(elMenor(3, 5, 4));
+
+//usamos spread porque se trabaja recibiendo por parametro arrays
+var colocaEnMedio = function (array1, array2) {
+  const medio = Math.floor(array1.length / 2); // Calculamos el centro
+  const nuevoArray = array1.slice(); // Hacemos una copia del primer array
+  nuevoArray.splice(medio, 0, ...array2); // Insertamos arr2 en el medio
+  return nuevoArray; // Devolvemos el nuevo array
+
+}
+
+alert(colocaEnMedio([1],[3,4,5]))
