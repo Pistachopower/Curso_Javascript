de9f0d5b0fc1320ca433df710cf598c8643dad13
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-11-29 12:57:59 +0100

    se construye el primer ejercicio

--- Cookie/ejercicio14.js
+++ Cookie/ejercicio14.js
@@ -1,6 +1,6 @@
 /*
 Crear   un   formulario   que   contiene   un   conjunto   de   casillas   de   verificación   
-con   diferentes tipos   de   cafés   ­   espresso,   cappuccino,   moka,   y   
+con   diferentes tipos   de   cafés espresso,   cappuccino,   moka,   y   
 así   sucesivamente.   Pida   al   usuario su   nombre   y   número   de   habitación   
 y   seleccionar   un   tipo   de   café.   Indicar   al   usuario   que va   a   enviar
 el   café   a   su   número   de   habitación.   Cree   las   cookie   para   recordar   
@@ -8,33 +8,43 @@ las preferencias   del   usuario.   La   próxima   vez   que   el   usuario  en
 página, en introduzca su nombre de usuario y habitación, se mostrará su café favorito. 
 Después de haber pedido 3 cafés, se le dirá que hay una tarifa especial y su próximo café será gratuito.
 */
-
 window.addEventListener("load", inicializar);
 
 function inicializar() {
-    
-    // Obtenemos el objeto formulario con todas las etiquetas
-    const formulario = document.getElementById('ordenFormulario');
+    //obtenemos el objeto del formulario
+    var formulario = document.getElementById('ordenFormulario');
 
-    //con el objeto formulario usamos la propiedad onsubmit (sirve cuando
-    //el usuario da click en submit o enter)
-    //dentro de ella usamos una función anonima 
-    formulario.onsubmit = function () {
+    // Cargar preferencias del usuario si existen
+    //cargarPreferencias();
+
+
+    formulario.onsubmit = function (event) {
+        event.preventDefault(); // Evitar el envío del formulario
+
         // Capturar los valores
-        const nombre = document.getElementById('nombre').value.trim();
-        const numHabitacion = document.getElementById('numHabitacion').value.trim();
+        var nombre = document.getElementById('nombre').value.trim();
+        var numHabitacion = document.getElementById('numHabitacion').value.trim();
 
+        //obtenemos el objeto bebida seleccionado en el radio
+        var bebidaSeleccionada = document.querySelector('input[name="bebidas"]:checked');
+
+        //seleccionamos el texto del radio seleccionado
+        var tipoCafe = bebidaSeleccionada.value;
+
+        document.cookie = `nombre=${nombre}; SameSite=None; Secure`;
+        document.cookie = `numHabitacion=${numHabitacion}; SameSite=None; Secure`;
+        document.cookie = `tipoCafe=${tipoCafe}; SameSite=None; Secure`;
         
-        const bebidaSeleccionada = document.querySelector('input[name="bebidas"]:checked');
 
+        //pensar en como extraerla, luego compararla si el valor que tienes en los campos y las cookies son iguales 
+        //
 
-
-    //guardamos el texto que vamos a mostrar
-    //var mensaje= mensaje.textContent = nombre + " se te va a llevar el café";
     }
 }
 
-function obtenerTipoCafe() {
-
+function mostrarInformacionPedido(nombre, numHabitacion, tipoCafe) {
+    //obtenemos la etiqueta para escribir 
+    var mostrarInformacion = document.getElementById('mostrarInformacion');
+    var mensaje= nombre + " su café favorito es " + tipoCafe + " y se lo enviará a la habitación " + numHabitacion;
 
 }
