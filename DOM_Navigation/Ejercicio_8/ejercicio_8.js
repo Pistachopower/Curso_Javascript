c6b26e697cfba262d5ded73e8a7626023c1296de
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-01-09 14:20:30 +0100

    se mejora el ejercicio

--- DOM_Navigation/Ejercicio_8/ejercicio_8.js
+++ DOM_Navigation/Ejercicio_8/ejercicio_8.js
@@ -1,32 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-  const viajes = [
+document.addEventListener("DOMContentLoaded", function () {
+  var viajes = [
     //esto es un array
-      { 
-        //clave: valor
-        src: "/img/img1.jpg", 
-        desc: "descripcion1", 
-        specs: ["spec11", "spec12"]
-      },
-      { 
-        src: "/img/img2.jpg", 
-        desc: "descripcion2", 
-        specs: ["spec21", "spec22"]
-      },
-    ];
-  
-    //seleccionamos la etiqueta completa con el hijo ul
+    {
+      //clave: valor
+      src: "/img/img1.jpg",
+      desc: "descripcion1",
+      specs: ["spec11", "spec12"]
+    },
+    {
+      src: "/img/img2.jpg",
+      desc: "descripcion2",
+      specs: ["spec21", "spec22"]
+    },
+  ];
 
-    /*
-    por cada indice del objeto va a ir pintando el contenido del json
-    */
-    const ul = document.querySelector("#todos-los-viajes ul");
-  
-    //recorremos el json
-    viajes.forEach(viaje => {
-      //creamos un elemento li nueva 
-      const li = document.createElement("li");
+  //llamamos a la funcion 
+  pintarContenido(viajes);
 
+});
+
+
+
+function pintarContenido(viajes) {
+
+
+  //tomamos la etiqueta completa y todos sus hijos 
+  var ul = document.querySelector("#todos-los-viajes ul");
+
+  //recorremos el array de objeto 
+  viajes.forEach(viaje => {
+
+  //pintamos la estructura de la etiqueta
+  var li= document.createElement("li");
+
+  
       //creamos un elemento img
       const img = document.createElement("img");
       img.src = viaje.src;
@@ -49,6 +56,7 @@ document.addEventListener("DOMContentLoaded", function() {
       li.appendChild(p);
       li.appendChild(specsList);
       ul.appendChild(li);
-    });
+
   });
-  
+
+}
